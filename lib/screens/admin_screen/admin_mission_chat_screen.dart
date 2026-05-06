110572482b10c21f068e8da3caf4ea846c84cb87
kabyrNdiaye <smknd2002@gmail.com>
2026-05-06 02:49:39 +0000

    Fix: Add missing services.dart import for SystemUiOverlayStyle

--- lib/screens/admin_screen/admin_mission_chat_screen.dart
+++ lib/screens/admin_screen/admin_mission_chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
